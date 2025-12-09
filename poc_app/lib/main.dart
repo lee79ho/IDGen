95c5c84584e2a14c44d38108879037f37cad5ad2
young <young@usimsa.com>
2025-12-09 11:35:35 +0900

    add string

--- poc_app/lib/main.dart
+++ poc_app/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package.flutter/material.dart';
+
+import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:poc_app/pages/splash_page.dart';
 
@@ -10,6 +11,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
 
   @override
   Widget build(BuildContext context) {
